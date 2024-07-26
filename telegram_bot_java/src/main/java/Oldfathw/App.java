1e8ddc34aeb3129706293153fff3ce7d2e08b404
Polina-Mikhaiovna <umrilovapolina@gmail.com>
2024-07-26 15:07:04 +0300

    Start of maven project

---
+++ telegram_bot_java/src/main/java/Oldfathw/App.java
@@ -0,0 +1,13 @@
+package Oldfathw;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
